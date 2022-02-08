638e88d8ae7ae39649f38396fa37561f72d337b2
Abid Al Wassie <abidalwassie@outlook.com>
2022-02-08 14:01:04 +0600

    updated project

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/orders', function () {
+    // get the data from db
+    return view('orders');
+});
